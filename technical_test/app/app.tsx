83add1a49cfb8dd38564ff2c246c1a522da2c646
Dararith <markwilhelm5540@gmail.com>
2026-02-04 16:31:50 +0700

    feature: login component

--- technical_test/app/app.tsx
+++ technical_test/app/app.tsx
@@ -1,14 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
+import Login from "@/components/login";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>test</Text>
+    <View>
+      <Login />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", justifyContent: "center" },
-  title: { fontSize: 28, fontWeight: "700" },
 });
